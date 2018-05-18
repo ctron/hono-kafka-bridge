eec8de1a8812581e9f4dbe6d5d42258833a38c2c
Jens Reimann <jreimann@redhat.com>
2018-05-18 18:15:19 +0200

    Process the metrics

--- src/main/java/de/dentrassi/iot/hono/bridge/ProcessMetrics.java
+++ src/main/java/de/dentrassi/iot/hono/bridge/ProcessMetrics.java
@@ -14,48 +14,44 @@ package de.dentrassi.iot.hono.bridge;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.qpid.proton.amqp.Symbol;
-import org.apache.qpid.proton.message.Message;
+import org.apache.camel.Message;
 import org.influxdb.dto.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component("metricsProcessor")
 public class ProcessMetrics {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProcessMetrics.class);
+
     public Point transform(final Message msg) {
 
         final Point.Builder p = Point.measurement("P").time(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
 
-        for (final Map.Entry<Symbol, ?> entry : msg.getMessageAnnotations().getValue().entrySet()) {
-            if (entry.getValue() instanceof String) {
-                p.tag(entry.getKey().toString(), "" + entry.getValue());
-                System.out.format("%s = %s%n", entry.getKey(), entry.getValue());
+        for (final Map.Entry<String, ?> entry : msg.getHeaders().entrySet()) {
+            if (!entry.getKey().startsWith("JMS") && entry.getValue() instanceof String) {
+                p.tag(entry.getKey(), (String) entry.getValue());
             }
         }
 
-        /*
-        final Map<?, ?> values = Json.decodeValue(string, Map.class);
-        
-        final Point.Builder p = Point.measurement("P").time(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
-        
-        for (final Map.Entry<Symbol, ?> entry : msg.getMessageAnnotations().getValue().entrySet()) {
-            if (entry.getValue() instanceof String) {
-                p.tag(entry.getKey().toString(), "" + entry.getValue());
-            }
+        final Map<?, ?> values = msg.getBody(Map.class);
+        if (values == null) {
+            return null;
         }
-        
+
         for (final Map.Entry<?, ?> entry : values.entrySet()) {
             if (!(entry.getKey() instanceof String)) {
                 continue;
             }
-        
+
             final String key = (String) entry.getKey();
             final Object value = entry.getValue();
-        
+
             if (value == null) {
                 continue;
             }
-        
+
             if (value instanceof Number) {
                 p.addField(key, (Number) value);
             } else if (value instanceof String) {
@@ -66,10 +62,8 @@ public class ProcessMetrics {
                 }
             }
         }
-        
-        this.db.write(p.build());*/
 
-        return null;
+        return p.build();
     }
 
 }
