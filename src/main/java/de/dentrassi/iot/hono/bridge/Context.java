8caded19cdde6eb6696930d4ee00d72adc97e115
Jens Reimann <jreimann@redhat.com>
2018-05-18 16:43:50 +0200

    Add basic influxdb

--- src/main/java/de/dentrassi/iot/hono/bridge/Context.java
+++ src/main/java/de/dentrassi/iot/hono/bridge/Context.java
@@ -12,6 +12,8 @@
 package de.dentrassi.iot.hono.bridge;
 
 import org.apache.camel.component.amqp.AMQPConnectionDetails;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +21,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Context {
 
-    private @Value("${amqp.uri}") String uri;
-    private @Value("${amqp.password}") String password;
-    private @Value("${amqp.username}") String username;
+    private @Value("${amqp.uri}") String amqpUri;
+    private @Value("${amqp.username}") String amqpUsername;
+    private @Value("${amqp.password}") String amqpPassword;
+
+    private @Value("${influx.uri}") String influxUrl;
+    private @Value("${influx.username}") String influxUsername;
+    private @Value("${influx.password}") String influxPassword;
 
     @Bean
     public AMQPConnectionDetails amqpConnection() {
-        return new AMQPConnectionDetails(this.uri, this.username, this.password);
+        return new AMQPConnectionDetails(this.amqpUri, this.amqpUsername, this.amqpPassword);
+    }
+
+    @Bean(name = "influxDbConnectionBean")
+    public InfluxDB influxDbConnection() {
+        return InfluxDBFactory.connect(this.influxUrl, this.influxUsername, this.influxPassword);
     }
 }
